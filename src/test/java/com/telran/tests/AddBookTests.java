bcf4f9ec312fc6f9a822eb8fa7ab5b6e3ff7178a
Elena Lebedeva <89071540+ElenaL171@users.noreply.github.com>
2022-10-04 00:34:05 +0200

    up

--- src/test/java/com/telran/tests/AddBookTests.java
+++ src/test/java/com/telran/tests/AddBookTests.java
@@ -1,5 +1,21 @@
 package com.telran.tests;
 
+import com.telran.data.BookData;
+import com.telran.pages.BookStorePage;
+import com.telran.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class AddBookTests extends TestBase{
-    
+    @BeforeMethod
+    public void ensurePrecondition(){
+        new HomePage(driver).selectBookStoreApp();
+        new BookStorePage(driver).clickOnLoginButton();
+    }
+    @Test
+    public void searchBookPositiveTestForRegisterUser(){
+        new BookStorePage(driver).typeInSearchBookInput(BookData.BOOK_NAME);
+        Assert.assertTrue(new BookStorePage(driver).takeNameOfBook().contains(BookData.BOOK_NAME));
+    }
 }
